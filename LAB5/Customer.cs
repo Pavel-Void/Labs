f7fef117e5735881ab724f1bbf95be988fc6b0b0
Void <pavel_sendv1k@mail.ru>
2025-01-22 04:45:55 +0500

    lab5

---
+++ LAB5/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooshopApp
+{
+    class Customer
+    {
+        int id;
+        string name;
+        int age;
+        string address;
+
+        public Customer(int id, string name, int age, string address)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public int GetID()
+        {
+            return id;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        public string GetAddress()
+        {
+            return address;
+        }
+
+        public override string ToString()
+        {
+            return $"{id} | {name} | {age} | {address}";
+        }
+    }
+}
---
+++ LAB5/Sales.cs
@@ -0,0 +1,57 @@
+﻿using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooshopApp
+{
+    class Sale
+    {
+        int id;
+        int id_animal;
+        int id_customer;
+        DateTime date;
+        decimal price;
+
+        public Sale(int id, int id_animal, int id_customer, DateTime date, decimal price)
+        {
+            this.id = id;
+            this.id_animal = id_animal;
+            this.id_customer = id_animal;
+            this.date = date;
+            this.price = price;
+        }
+
+        public int GetID()
+        {
+            return id;
+        }
+
+        public int GetIdAnimals()
+        {
+            return id_animal;
+        }
+
+        public int GetIdCustomer()
+        {
+            return id_customer;
+        }
+
+        public DateTime GetDate()
+        {
+            return date;
+        }
+
+        public decimal GetPrice()
+        {
+            return price;
+        }
+
+        public override string ToString()
+        {
+            return $"{id} | {id_animal} | {id_customer} | {date} | {price}";
+        }
+    }
+}
